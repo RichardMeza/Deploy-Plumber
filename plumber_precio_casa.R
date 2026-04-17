69fa319787ecfd436dd6f3074eab56639b9e29d7
Aldo Richard Meza Rodríguez <145654581+RichardMeza@users.noreply.github.com>
2026-04-17 18:51:56 -0500

    Update plumber_precio_casa.R

--- plumber_precio_casa.R
+++ plumber_precio_casa.R
@@ -1,15 +1,12 @@
 library(plumber)
-library(tidymodels)
 library(recipes)
 library(tibble)
 
-# Cargar objetos guardados
 model <- readRDS("modelo_precio_rf.rds")
 transformer_var <- readRDS("transformer_var.rds")
 
 #* @apiTitle Prediccion del precio de la casa
 
-#* Conectar a la api
 #* @get /connection-status
 function() {
   list(
@@ -18,14 +15,13 @@ function() {
   )
 }
 
-#* Predecir el precio de una casa
-#* @param precio_terreno precio del terreno
-#* @param metros_habitables metros habitables
-#* @param banios cantidad de banios
-#* @param antiguedad antiguedad de la casa
-#* @param metros_totales metros de la casa
-#* @param dormitorios cantidad de dormitorios
 #* @get /predict
+#* @param precio_terreno
+#* @param metros_habitables
+#* @param banios
+#* @param antiguedad
+#* @param metros_totales
+#* @param dormitorios
 function(precio_terreno, metros_habitables, banios,
          antiguedad, metros_totales, dormitorios) {
 
@@ -41,8 +37,6 @@ function(precio_terreno, metros_habitables, banios,
   trans_df <- bake(transformer_var, new_data = new_df)
   pred <- predict(model, new_data = trans_df)
 
-  list(
-    prediccion = as.numeric(pred$.pred)
-  )
+  list(prediccion = as.numeric(pred$.pred))
 }
 
