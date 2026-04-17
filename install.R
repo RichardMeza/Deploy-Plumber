18fe59ec10e42df89ee4f6480ffca522f0ed8f7e
Aldo Richard Meza Rodríguez <145654581+RichardMeza@users.noreply.github.com>
2026-04-17 15:47:07 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,20 @@
-install.packages(c(
-  "plumber",
-  "jsonlite",
-  "randomForest",
-  "caret"
-))
+# Instalar paquetes si no existen
+packages <- c("plumber", "jsonlite", "randomForest", "caret")
+
+for (p in packages) {
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p, repos = "https://cloud.r-project.org")
+    library(p, character.only = TRUE)
+  }
+}
+
+library(plumber)
+
+pr <- plumb("plumber_precio_casa.R")
+
+port <- as.numeric(Sys.getenv("PORT", "10000"))
+
+pr$run(
+  host = "0.0.0.0",
+  port = port
+)
