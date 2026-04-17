b1771c41c04782b5552fc11b4bf0fa57e13d08d5
Aldo Richard Meza Rodríguez <145654581+RichardMeza@users.noreply.github.com>
2026-04-17 18:05:49 -0500

    Update plumber_precio_casa.R

--- plumber_precio_casa.R
+++ plumber_precio_casa.R
@@ -1,35 +1,21 @@
-library(tidymodels)
 library(plumber)
-library(caret)
-library(jsonlite)
-library(ggpubr)
+library(tidymodels)
 library(recipes)
-library(readr)
-library(conflicted)
-conflict_prefer("filter", "dplyr")
-#--------------------------------------------------
-# Read in model 
-#--------------------------------------------------
-setwd("C:/Users/armez/Documents/plumber/ngrok-v3-stable-windows-amd64")
-model <- readr::read_rds("./modelo_precio_rf.rds")
-summary(model)
-transformer_var <- readr::read_rds("./transformer_var.rds")
+library(tibble)
 
-new_df <- data.frame(precio_terreno=4, metros_habitables=4,
-                       banios=5, antiguedad=6, metros_totales=7,
-                       dormitorios=8)
-trans_df <- data.frame(bake(transformer_var, new_data = new_df))
-predict(model, trans_df)
+# Cargar objetos guardados
+model <- readRDS("modelo_precio_rf.rds")
+transformer_var <- readRDS("transformer_var.rds")
 
 #* @apiTitle Prediccion del precio de la casa
 
 #* Conectar a la api
 #* @get /connection-status
-
-function(){
-  list(status = "Conexion exitosa a la API del precio de la casa", 
-       time = Sys.time(),
-       username = Sys.getenv("USERNAME"))
+function() {
+  list(
+    status = "Conexion exitosa a la API del precio de la casa",
+    time = Sys.time()
+  )
 }
 
 #* Predecir el precio de una casa
@@ -38,18 +24,25 @@ function(){
 #* @param banios cantidad de banios
 #* @param antiguedad antiguedad de la casa
 #* @param metros_totales metros de la casa
-#* @param dormitorios cantidad de dormitores
+#* @param dormitorios cantidad de dormitorios
 #* @get /predict
+function(precio_terreno, metros_habitables, banios,
+         antiguedad, metros_totales, dormitorios) {
 
-function(precio_terreno, metros_habitables,banios,
-         antiguedad, metros_totales, dormitorios){
-  new_df <- tibble(precio_terreno = as.numeric(precio_terreno),
-                   metros_habitables = as.numeric(metros_habitables),
-                   banios = as.numeric(banios), 
-                   antiguedad = as.numeric(antiguedad),
-                   metros_totales = as.numeric(metros_totales),
-                   dormitorios = as.numeric(dormitorios))
-  trans_df <- tibble(bake(transformer_var, new_data = new_df))
-  predict(model, new_data =trans_df)
+  new_df <- tibble(
+    precio_terreno = as.numeric(precio_terreno),
+    metros_habitables = as.numeric(metros_habitables),
+    banios = as.numeric(banios),
+    antiguedad = as.numeric(antiguedad),
+    metros_totales = as.numeric(metros_totales),
+    dormitorios = as.numeric(dormitorios)
+  )
+
+  trans_df <- bake(transformer_var, new_data = new_df)
+  pred <- predict(model, new_data = trans_df)
+
+  list(
+    prediccion = as.numeric(pred$.pred)
+  )
 }
 
