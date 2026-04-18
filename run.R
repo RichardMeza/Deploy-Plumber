597e50c84da7b9f1239b565a8f382b1af224a3e9
Aldo Richard Meza Rodríguez <145654581+RichardMeza@users.noreply.github.com>
2026-04-17 19:01:11 -0500

    Update run.R

--- run.R
+++ run.R
@@ -1,18 +1,12 @@
-packages <- c("plumber", "recipes", "tibble")
+library(plumber)
+library(recipes)
+library(tibble)
 
-for (p in packages) {
-  if (!require(p, character.only = TRUE)) {
-    install.packages(p, repos = "https://cloud.r-project.org")
-    library(p, character.only = TRUE)
-  }
-}
+pr <- plumb("plumber_precio_casa.R")
 
-tryCatch({
-  pr <- plumb("plumber_precio_casa.R")
-  port <- as.numeric(Sys.getenv("PORT", "10000"))
-  pr$run(host = "0.0.0.0", port = port)
-}, error = function(e) {
-  message("ERROR DETECTADO EN PRODUCCION:")
-  message(conditionMessage(e))
-  quit(status = 1)
-})
+port <- as.numeric(Sys.getenv("PORT", "10000"))
+
+pr$run(
+  host = "0.0.0.0",
+  port = port
+)
